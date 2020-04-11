91ddab73231fe78441b56f7b27b1ac9edbbb13de
prikhodkin <prikhodkin_ilya@mail.ru>
2020-04-12 01:11:27 +0500

    Закончил каталог и форму связи

--- src/blocks/promo/promo.js
+++ src/blocks/promo/promo.js
@@ -4,5 +4,13 @@ $('.promo__list').slick({
   dots: true,
   adaptiveHeight: true,
   nextArrow: '.promo__arrow--right',
-  prevArrow: '.promo__arrow--left'
+  prevArrow: '.promo__arrow--left',
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        arrows: false
+      }
+    },
+  ]
 });
